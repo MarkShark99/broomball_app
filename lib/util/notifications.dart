497924f7894075882c134e57c5b40cbc3bcb1333
mfthor <43010547+mfthor@users.noreply.github.com>
2019-12-06 12:43:22 -0500

    added more functions to util/notifications

--- lib/util/notifications.dart
+++ lib/util/notifications.dart
@@ -1,16 +1,26 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
 
 class BroomballNotification {
-
   static final BroomballNotification _instance = BroomballNotification._internal();
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
+  AndroidNotificationDetails androidPlatformChannelSpecifics;
+  NotificationDetails platformChannelSpecifics;
 
-  void init() {
+  void initNotifications() {
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
-      // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-      var initializationSettingsAndroid =
-          new AndroidInitializationSettings('app_icon');
-      var initializationSettings = InitializationSettings(initializationSettingsAndroid, null);
-      flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+    var initializationSettings = InitializationSettings(initializationSettingsAndroid, null);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+  }
+
+  void initDisplay(){
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails('your channel id', 'your channel name', 'your channel description', importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var platformChannelSpecifics = NotificationDetails(
+    androidPlatformChannelSpecifics, null);
+
   }
 
   factory BroomballNotification() {
@@ -20,10 +30,20 @@ class BroomballNotification {
   BroomballNotification._internal();
 
   Future onSelectNotification(String payload) async {
-    //if (payload != null) {
-    //  debugPrint('notification payload: ' + payload);
-    //}
-    //await 
+    // {
+    // Navigator.of(context).push(MaterialPageRoute(
+    // builder: (context) {
+    // return PlayerPage(id: id);
+    //},
+    // ));
+    // }
     print("Pressed");
   }
+
+  Future convertScheduleNotification(String matchTime) async {
+
+
+
+  }
+
 }
