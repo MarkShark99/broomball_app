279ffeaad4404bd690f15dc46d8082ee64f8e079
mfthor <43010547+mfthor@users.noreply.github.com>
2019-12-06 09:59:54 -0500

    added functions to util/notifications

--- lib/util/notifications.dart
+++ lib/util/notifications.dart
@@ -1,15 +1,25 @@
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class BroomballNotification {
-  static void init() {
+
+  static final BroomballNotification _instance = BroomballNotification._internal();
+
+  void init() {
     FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = new FlutterLocalNotificationsPlugin();
       // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
-      // var initializationSettingsAndroid =
-          // new AndroidInitializationSettings('app_icon');
-      // var initializationSettings = InitializationSettings(initializationSettingsAndroid, null);
-      // flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
+      var initializationSettingsAndroid =
+          new AndroidInitializationSettings('app_icon');
+      var initializationSettings = InitializationSettings(initializationSettingsAndroid, null);
+      flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: onSelectNotification);
   }
-  static Future onSelectNotification(String payload) async {
+
+  factory BroomballNotification() {
+    return _instance;
+  }
+
+  BroomballNotification._internal();
+
+  Future onSelectNotification(String payload) async {
     //if (payload != null) {
     //  debugPrint('notification payload: ' + payload);
     //}
